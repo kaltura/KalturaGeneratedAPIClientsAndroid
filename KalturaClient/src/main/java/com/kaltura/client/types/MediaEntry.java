72b6dffa33d2728f6f2fce3925fd2cd5a32af377
Kaltura CSI <community@kaltura.com>
2017-12-27 03:40:54 -0500

    New clientlib ver

--- KalturaClient/src/main/java/com/kaltura/client/types/MediaEntry.java
+++ KalturaClient/src/main/java/com/kaltura/client/types/MediaEntry.java
@@ -63,8 +63,6 @@ public class MediaEntry extends PlayableEntry {
 		String flavorParamsIds();
 		String isTrimDisabled();
 		RequestBuilder.ListTokenizer<StreamContainer.Tokenizer> streams();
-		String isSequenceEntry();
-		String sequenceEntryIds();
 	}
 
 	/**
@@ -116,14 +114,6 @@ public class MediaEntry extends PlayableEntry {
 	 * Array of streams that exists on the entry
 	 */
 	private List<StreamContainer> streams;
-	/**
-	 * True if the entry is a sequence entry
-	 */
-	private Boolean isSequenceEntry;
-	/**
-	 * The sequence entries of the entry
-	 */
-	private String sequenceEntryIds;
 
 	// mediaType:
 	public MediaType getMediaType(){
@@ -265,30 +255,6 @@ public class MediaEntry extends PlayableEntry {
 		this.streams = streams;
 	}
 
-	// isSequenceEntry:
-	public Boolean getIsSequenceEntry(){
-		return this.isSequenceEntry;
-	}
-	public void setIsSequenceEntry(Boolean isSequenceEntry){
-		this.isSequenceEntry = isSequenceEntry;
-	}
-
-	public void isSequenceEntry(String multirequestToken){
-		setToken("isSequenceEntry", multirequestToken);
-	}
-
-	// sequenceEntryIds:
-	public String getSequenceEntryIds(){
-		return this.sequenceEntryIds;
-	}
-	public void setSequenceEntryIds(String sequenceEntryIds){
-		this.sequenceEntryIds = sequenceEntryIds;
-	}
-
-	public void sequenceEntryIds(String multirequestToken){
-		setToken("sequenceEntryIds", multirequestToken);
-	}
-
 
 	public MediaEntry() {
 		super();
@@ -312,8 +278,6 @@ public class MediaEntry extends PlayableEntry {
 		flavorParamsIds = GsonParser.parseString(jsonObject.get("flavorParamsIds"));
 		isTrimDisabled = GsonParser.parseBoolean(jsonObject.get("isTrimDisabled"));
 		streams = GsonParser.parseArray(jsonObject.getAsJsonArray("streams"), StreamContainer.class);
-		isSequenceEntry = GsonParser.parseBoolean(jsonObject.get("isSequenceEntry"));
-		sequenceEntryIds = GsonParser.parseString(jsonObject.get("sequenceEntryIds"));
 
 	}
 
@@ -328,8 +292,6 @@ public class MediaEntry extends PlayableEntry {
 		kparams.add("creditUserName", this.creditUserName);
 		kparams.add("creditUrl", this.creditUrl);
 		kparams.add("streams", this.streams);
-		kparams.add("isSequenceEntry", this.isSequenceEntry);
-		kparams.add("sequenceEntryIds", this.sequenceEntryIds);
 		return kparams;
 	}
 
@@ -366,8 +328,6 @@ public class MediaEntry extends PlayableEntry {
         } else {
             dest.writeInt(-1);
         }
-        dest.writeBoolean(this.isSequenceEntry);
-        dest.writeString(this.sequenceEntryIds);
     }
 
     public MediaEntry(Parcel in) {
@@ -391,8 +351,6 @@ public class MediaEntry extends PlayableEntry {
             this.streams = new ArrayList<>();
             in.readList(this.streams, StreamContainer.class.getClassLoader());
         }
-        this.isSequenceEntry = in.readBoolean();
-        this.sequenceEntryIds = in.readString();
     }
 }
 
